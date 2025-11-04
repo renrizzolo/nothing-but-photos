738cd136a384e7ecf0aa70b66137178180e6d3de
Ren Rizzolo <renrizmusic@gmail.com>
2025-11-05 08:48:12 +1100

    upgrade node in workflows

--- test/setup.ts
+++ test/setup.ts
@@ -11,19 +11,14 @@ afterEach(() => {
 
 // fix jsdom missing pointer capture methods
 beforeAll(() => {
-  if (typeof window !== "undefined") {
-    if (
-      typeof Element !== "undefined" &&
-      !Element.prototype.setPointerCapture
-    ) {
-      Element.prototype.setPointerCapture = vi.fn();
-    }
-    if (
-      typeof Element !== "undefined" &&
-      !Element.prototype.releasePointerCapture
-    ) {
-      Element.prototype.releasePointerCapture = vi.fn();
-    }
+  if (typeof Element !== "undefined" && !Element.prototype.setPointerCapture) {
+    Element.prototype.setPointerCapture = vi.fn();
+  }
+  if (
+    typeof Element !== "undefined" &&
+    !Element.prototype.releasePointerCapture
+  ) {
+    Element.prototype.releasePointerCapture = vi.fn();
   }
 });
 
